df951a01a353e8c6755e2cd1a60c07270444f5f1
Ahmed Mohamed <114872733+GEMIv1@users.noreply.github.com>
2026-01-03 22:39:23 +0200

    Delete NeuralNetwork directory

--- NeuralNetwork/src/main/java/com/example/init/IWeightInitializer.java
+++
@@ -1,7 +0,0 @@
-package com.example.init;
-
-public interface IWeightInitializer {
-
-    // Input: number of input neurons from the prev layer, number of output neurons in the current layer
-    public double[][] init(int noInputs, int noOutputs);
-}
